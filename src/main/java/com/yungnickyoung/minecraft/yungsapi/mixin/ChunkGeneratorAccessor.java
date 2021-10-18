fe0fbb330a375a59ca4be7dee96a5c45455345ba
Nick Young <yungnickyoung@gmail.com>
2021-10-18 00:50:18 -0400

    Build Forge-13. Add Mutable annotation to mixins to make it compatible w/ Java 9+.

--- src/main/java/com/yungnickyoung/minecraft/yungsapi/mixin/ChunkGeneratorAccessor.java
+++ src/main/java/com/yungnickyoung/minecraft/yungsapi/mixin/ChunkGeneratorAccessor.java
@@ -4,11 +4,13 @@ import com.mojang.serialization.Codec;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.settings.DimensionStructuresSettings;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ChunkGenerator.class)
 public interface ChunkGeneratorAccessor {
+    @Mutable
     @Accessor("settings")
     void yungsapi_setSettings(DimensionStructuresSettings dimensionStructuresSettings);
 
