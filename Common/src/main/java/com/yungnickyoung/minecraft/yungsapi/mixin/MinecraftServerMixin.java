2de5d029acfdd23f40301daf8b33a37081683abb
KJP12 <kjp12.k.joy.programer12@gmail.com>
2022-04-04 08:50:06 -0500

    Revert `require = 0` as dev will hang if conflicting mods are present. As such, both YUNG's API and ServerCore will conflict, but only when both are present in a development environment.

--- Common/src/main/java/com/yungnickyoung/minecraft/yungsapi/mixin/MinecraftServerMixin.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungsapi/mixin/MinecraftServerMixin.java
@@ -7,12 +7,12 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value = MinecraftServer.class, priority = 500)
 public class MinecraftServerMixin {
-    @ModifyConstant(method = "prepareLevels", constant = @Constant(intValue = 11, ordinal = 0), require = 0)
+    @ModifyConstant(method = "prepareLevels", constant = @Constant(intValue = 11, ordinal = 0))
     private static int fastSpawn(int constant) {
         return 0;
     }
 
-    @ModifyConstant(method = "prepareLevels", constant = @Constant(intValue = 441, ordinal = 0), require = 0)
+    @ModifyConstant(method = "prepareLevels", constant = @Constant(intValue = 441, ordinal = 0))
     private static int fastSpawn2(int constant) {
         return 0;
     }
