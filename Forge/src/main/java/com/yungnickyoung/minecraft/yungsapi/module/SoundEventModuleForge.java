350358a5d7fae684920eff93872d8e784bccaaa2
Nick Young <yungnickyoung@gmail.com>
2022-10-19 13:52:33 -0400

    v3.8.1. Fix broken SoundEvent registration on Forge.

--- Forge/src/main/java/com/yungnickyoung/minecraft/yungsapi/module/SoundEventModuleForge.java
+++ Forge/src/main/java/com/yungnickyoung/minecraft/yungsapi/module/SoundEventModuleForge.java
@@ -1,46 +1,32 @@
 package com.yungnickyoung.minecraft.yungsapi.module;
 
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegisterSoundEvent;
-import com.yungnickyoung.minecraft.yungsapi.autoregister.AutoRegistrationManager;
 import com.yungnickyoung.minecraft.yungsapi.autoregister.AutoRegisterField;
+import com.yungnickyoung.minecraft.yungsapi.autoregister.AutoRegistrationManager;
+import net.minecraft.core.Registry;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-
-import java.util.HashMap;
-import java.util.Map;
+import net.minecraftforge.registries.RegisterEvent;
 
 /**
  * Registration of Sound Events.
  */
 public class SoundEventModuleForge {
-    private static final Map<String, DeferredRegister<SoundEvent>> registersByModId = new HashMap<>();
-
     public static void processEntries() {
-        AutoRegistrationManager.SOUND_EVENTS.forEach(SoundEventModuleForge::register);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(SoundEventModuleForge::registerSoundEvents);
     }
 
-    private static void register(AutoRegisterField data) {
-        // Create & register deferred registry for current mod, if necessary
-        String modId = data.name().getNamespace();
-        if (!registersByModId.containsKey(modId)) {
-            DeferredRegister<SoundEvent> deferredRegister = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, modId);
-            deferredRegister.register(FMLJavaModLoadingContext.get().getModEventBus());
-            registersByModId.put(modId, deferredRegister);
-        }
+    private static void registerSoundEvents(RegisterEvent event) {
+        event.register(Registry.SOUND_EVENT_REGISTRY, helper -> AutoRegistrationManager.SOUND_EVENTS.stream()
+                .filter(data -> !data.processed())
+                .forEach(data -> registerSoundEvent(data, helper)));
+    }
 
+    private static void registerSoundEvent(AutoRegisterField data, RegisterEvent.RegisterHelper<SoundEvent> helper) {
         AutoRegisterSoundEvent autoRegisterSoundEvent = (AutoRegisterSoundEvent) data.object();
         SoundEvent soundEvent = new SoundEvent(data.name());
-
-        // Register
-        DeferredRegister<SoundEvent> deferredRegister = registersByModId.get(modId);
-        RegistryObject<SoundEvent> registryObject = deferredRegister.register(data.name().getPath(), () -> soundEvent);
-
-        // Update the supplier to use the RegistryObject so that it will be properly updated later on
-        autoRegisterSoundEvent.setSupplier(registryObject);
-
+        helper.register(data.name(), soundEvent);
+        autoRegisterSoundEvent.setSupplier(() -> soundEvent);
         data.markProcessed();
     }
 }
