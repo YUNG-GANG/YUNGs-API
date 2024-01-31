006bead02c12dea170579db3e56927a7f07611d8
Nick Young <yungnickyoung@gmail.com>
2024-01-30 20:52:19 -0500

    v4.0.3. Fix commands using AutoRegisterCommand not always registering.

--- Forge/src/main/java/com/yungnickyoung/minecraft/yungsapi/module/CommandModuleForge.java
+++ Forge/src/main/java/com/yungnickyoung/minecraft/yungsapi/module/CommandModuleForge.java
@@ -1,6 +1,7 @@
 package com.yungnickyoung.minecraft.yungsapi.module;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.yungnickyoung.minecraft.yungsapi.YungsApiCommon;
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegisterCommand;
 import com.yungnickyoung.minecraft.yungsapi.autoregister.AutoRegistrationManager;
 import com.yungnickyoung.minecraft.yungsapi.autoregister.AutoRegisterField;
@@ -20,7 +21,7 @@ public class CommandModuleForge {
 
     private static void registerCommands(RegisterCommandsEvent event) {
         AutoRegistrationManager.COMMANDS.stream()
-                .filter(data -> !data.processed())
+//                .filter(data -> !data.processed()) // We intentionally do not check for processing here, as we want to register the command every time the event is fired.
                 .forEach(data -> registerCommand(data, event.getDispatcher(), event.getBuildContext(), event.getCommandSelection()));
     }
 
@@ -30,6 +31,7 @@ public class CommandModuleForge {
                                         Commands.CommandSelection selection) {
         AutoRegisterCommand autoRegisterCommand = (AutoRegisterCommand) data.object();
         autoRegisterCommand.invokeHandler(dispatcher, context, selection);
-        data.markProcessed();
+
+        // data.markProcessed();
     }
 }
