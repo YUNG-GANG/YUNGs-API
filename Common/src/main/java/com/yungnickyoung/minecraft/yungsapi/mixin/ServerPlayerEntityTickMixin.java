15f445842ca1dad9699e5805d6c7c24ee3dcadd4
Nick Young <yungnickyoung@gmail.com>
2024-02-17 18:30:45 -0500

    v4.4.0. Port to 1.20.4. Remove safe structure locate trigger. Remove .idea dir from git.

--- Common/src/main/java/com/yungnickyoung/minecraft/yungsapi/mixin/ServerPlayerEntityTickMixin.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungsapi/mixin/ServerPlayerEntityTickMixin.java
@@ -1,29 +1,15 @@
 package com.yungnickyoung.minecraft.yungsapi.mixin;
 
 import com.mojang.authlib.GameProfile;
-import com.yungnickyoung.minecraft.yungsapi.module.CriteriaModule;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerEntityTickMixin extends Player {
     public ServerPlayerEntityTickMixin(Level $$0, BlockPos $$1, float $$2, GameProfile $$3) {
         super($$0, $$1, $$2, $$3);
     }
-
-    /**
-     * Checks for custom triggers.
-     */
-//    @Inject(method = "tick", at = @At("HEAD"))
-//    private void yungsapi_tickCriteria(CallbackInfo info) {
-//        if (this.tickCount % 20 == 0) {
-//            CriteriaModule.SAFE_STRUCTURE_LOCATION.trigger(this);
-//        }
-//    }
 }
