c49b34c9bf47e58fdb4040d778238ab0eee28c27
Nick Young <yungnickyoung@gmail.com>
2025-03-03 21:52:39 -0500

    v5.1.4. Add mixin to prevent Jukebox entities from crashing due to null level.

---
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungsapi/mixin/FixJukeboxCrashMixin.java
@@ -0,0 +1,30 @@
+package com.yungnickyoung.minecraft.yungsapi.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(JukeboxBlockEntity.class)
+public abstract class FixJukeboxCrashMixin extends BlockEntity {
+    public FixJukeboxCrashMixin(BlockEntityType<?> $$0, BlockPos $$1, BlockState $$2) {
+        super($$0, $$1, $$2);
+    }
+
+    @Inject(method = "setTheItem",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/Level;registryAccess()Lnet/minecraft/core/RegistryAccess;"),
+            cancellable = true)
+    public void yungsapi_checkIfLevelNull(ItemStack itemStack, CallbackInfo ci) {
+        if (this.level == null) {
+            ci.cancel();
+        }
+    }
+}
