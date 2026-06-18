5afd78e23606ede89aa9fe19480aa4ef33a843be
williambl <william@williambl.com>
2026-06-18 11:51:12 +0100

    Fix enhanced beardifier not running when vanilla beardifier's affectedBox is null

--- Common/src/main/java/com/yungnickyoung/minecraft/yungsapi/mixin/BeardifierMixin.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungsapi/mixin/BeardifierMixin.java
@@ -11,18 +11,20 @@ import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.levelgen.Beardifier;
 import net.minecraft.world.level.levelgen.DensityFunction;
+import net.minecraft.world.level.levelgen.DensityFunctions;
 import net.minecraft.world.level.levelgen.NoiseChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Injects behavior required for using {@link EnhancedTerrainAdaptation} with {@link YungJigsawStructure}.
  */
 @Mixin(Beardifier.class)
-public class BeardifierMixin implements EnhancedBeardifierData {
+public abstract class BeardifierMixin implements EnhancedBeardifierData, DensityFunctions.BeardifierOrMarker {
     @Unique
     private ObjectListIterator<EnhancedJigsawJunction> enhancedJunctionIterator;
 
@@ -36,6 +38,14 @@ public class BeardifierMixin implements EnhancedBeardifierData {
     private static void yungsapi_supportCustomTerrainAdaptations(StructureManager structureManager, ChunkPos chunkPos, CallbackInfoReturnable<Beardifier> cir) {
         Beardifier enhancedBeardifier = EnhancedBeardifierHelper.forStructuresInChunk(structureManager, chunkPos, cir.getReturnValue());
         cir.setReturnValue(enhancedBeardifier);
+    }
+
+    @Inject(method = "fillArray", at = @At(value = "INVOKE", target = "Ljava/util/Arrays;fill([DD)V"), cancellable = true)
+    private void yungsapi_fillArray(final double[] output, final DensityFunction.ContextProvider contextProvider, final CallbackInfo ci) {
+        if (this.enhancedPieceIterator.hasNext() || this.enhancedJunctionIterator.hasNext()) {
+            DensityFunctions.BeardifierOrMarker.super.fillArray(output, contextProvider);
+            ci.cancel();
+        }
     }
 
     @Inject(method = "compute", at = @At("RETURN"), cancellable = true)
