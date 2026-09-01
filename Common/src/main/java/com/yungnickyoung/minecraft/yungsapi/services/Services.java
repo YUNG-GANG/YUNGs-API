3a125fda2fb2e057fd2f186b03f2dda8e531130d
williambl <william@williambl.com>
2026-09-01 11:09:34 +0100

    Fix unsafe call to ServiceLoader#load
    See jaredlll08/MultiLoader-Template#97
    
    See YUNG-GANG/YUNGs-Better-Caves#198

--- Common/src/main/java/com/yungnickyoung/minecraft/yungsapi/services/Services.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungsapi/services/Services.java
@@ -11,7 +11,7 @@ public class Services {
     public static final IParticleTypeHelper PARTICLE_HELPER = load(IParticleTypeHelper.class);
 
     public static <T> T load(Class<T> clazz) {
-        final T loadedService = ServiceLoader.load(clazz)
+        final T loadedService = ServiceLoader.load(clazz, Services.class.getClassLoader())
                 .findFirst()
                 .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
         YungsApiCommon.LOGGER.debug("Loaded {} for service {}", loadedService, clazz);
